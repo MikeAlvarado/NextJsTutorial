67cb07c84629c6ab3a16a4d634f781a339ea37a3
MikeAlvarado <miguel_l06@hotmail.com>
2020-08-25 11:49:01 -0500

    Created NextJS App + Navigation + Assets and Stylings

---
+++ nextjs-blog/pages/index.js
@@ -0,0 +1,20 @@
+import Head from 'next/head'
+import Layout, { siteTitle } from '../components/layout'
+import utilStyles from '../styles/utils.module.css'
+
+export default function Home() {
+  return (
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <p>[Software Engineer - Passionate about UI, adept of UX, trying to make software more accessible and usable one line of code at a time.]</p>
+        <p>
+          (This is a sample website - you’ll be building a site like this on{' '}
+          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+        </p>
+      </section>
+    </Layout>
+  )
+}
